438bc5b2f4abb6620f453fd560514ea386114caa
subloger <subloger@rambler.ru>
2022-02-14 21:26:09 +0300

    home work for lesson-3

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 import json
 
 from django.shortcuts import render
+from .models import ProductCategory, Product
 
 with open('products/fixtures/products.json', 'r', encoding='utf-8') as f:
     prod = json.load(f)
@@ -13,8 +14,10 @@ def index(request):
 
 
 def products(request):
+    ppp = ProductCategory.objects.all
     context = {
         'title': 'GeekShop - Каталог',
+        'productcategory': ppp,
         'products': prod
     }
     return render(request, 'products/products.html', context)
