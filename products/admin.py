ce5ea39ff2e7a1b05e56f0aeb874e7eede88890c
subloger <subloger@rambler.ru>
2022-03-02 23:42:26 +0300

    Home work for lesson-7

--- products/admin.py
+++ products/admin.py
@@ -4,4 +4,11 @@ from .models import ProductCategory, Product
 # Register your models here.
 
 admin.site.register(ProductCategory)
-admin.site.register(Product)
+#admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'image', 'description', 'price', 'quantity', 'category')
+    search_fields = ('name',)
+    ordering = ('name',)
